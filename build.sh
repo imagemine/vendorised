3738463235cff0603120e1ca4070ab5c99a6a3bc
imageminer <imagemine2021@gmail.com>
2021-09-22 22:24:38 +1000

    container image snapshot workload

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+for item in $(cat workload.txt|grep ready);
+do
+  read source target <<< $(echo $item|awk -F"," '{print $1" "$2}')
+  docker pull ${source}
+  echo "tagging ${source} as ${target}"
+  docker tag ${source} ${target}
+  echo "pushing ${target}"
+  docker push ${target}
+done;
+
+
